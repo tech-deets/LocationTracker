451c36ba46bb491ecb8a7a87dc8696b8731334e8
tech deets <tech.deets@gamil.com>
2018-10-31 10:19:54 +0530

    30night

--- app/src/main/java/com/example/satnamsingh/locationtracker/ProfilePhoto.java
+++ app/src/main/java/com/example/satnamsingh/locationtracker/ProfilePhoto.java
@@ -60,6 +60,8 @@ public class ProfilePhoto extends AppCompatActivity {
                 databaseReference=firebaseDatabase.getReference("Users");
                 databaseReference.child(phoneNumber).child("photo").
                         setValue("https://firebasestorage.googleapis.com/v0/b/locationtracker-28250.appspot.com/o/default_pic.jpg?alt=media&token=7ed5b932-09bf-422e-aa5e-77eca30f378f");
+                Intent uin = new Intent(getApplicationContext(),UserHomeActivity.class);
+                startActivity(uin);
                 finish();
         }
         );
@@ -158,6 +160,8 @@ public class ProfilePhoto extends AppCompatActivity {
                             databaseReference.child(phoneNumber).child("photo").setValue(photo);
                             pd.dismiss();
                             Toast.makeText(ProfilePhoto.this, "Photo uploaded successfully", Toast.LENGTH_SHORT).show();
+                            Intent in = new Intent(getApplicationContext(),UserHomeActivity.class);
+                            startActivity(in);
                             finish();
                         }
                     });
