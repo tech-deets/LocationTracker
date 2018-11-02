2010b04d6f68019eb1c5aa26c1265cd2582babc4
tech deets <tech.deets@gmail.com>
2018-11-02 11:29:57 +0530

    groupdialog actvity added

---
+++ app/src/main/java/com/example/satnamsingh/locationtracker/GroupInfoDialogActivity.java
@@ -0,0 +1,13 @@
+package com.example.satnamsingh.locationtracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GroupInfoDialogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_info_dialog);
+    }
+}
